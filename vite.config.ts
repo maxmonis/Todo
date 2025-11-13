c41001a4263338185f2b855603f55fd2a323d405
Max <mmonis77@gmail.com>
2025-11-13 14:09:17 -0500

    Remove nitro code for now

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import tailwindcss from "@tailwindcss/vite"
-import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin"
 import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import viteReact from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
@@ -7,7 +6,6 @@ import viteTsConfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   plugins: [
-    nitroV2Plugin(),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
