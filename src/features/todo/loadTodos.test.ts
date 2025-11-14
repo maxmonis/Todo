2377aee8bc852d9d5ca9c5aaecf3251b4d8afef1
Max <mmonis77@gmail.com>
2025-11-14 01:24:38 -0500

    Add tests for loadTodos

---
+++ src/features/todo/loadTodos.test.ts
@@ -0,0 +1,36 @@
+import { expect, it, vi } from "vitest"
+import { db } from "~/server/mongoose"
+import { loadTodos } from "./loadTodos"
+
+vi.mock("@tanstack/react-start", () => {
+  return {
+    createServerFn: vi.fn(() => {
+      return {
+        middleware: vi.fn(() => {
+          return {
+            handler: vi.fn((cb: Function) => {
+              return vi.fn(() => cb({ context: { userId: "mockUserId" } }))
+            }),
+          }
+        }),
+      }
+    }),
+  }
+})
+vi.mock("~/server/mongoose")
+vi.mock("../auth/authMiddleware", () => {
+  return { authMiddleware: vi.fn() }
+})
+
+it("returns todos from DB", async () => {
+  vi.mocked(db.Todo.find).mockReturnValueOnce({
+    lean: vi.fn().mockResolvedValueOnce([
+      { _id: "washcarid", text: "Wash car" },
+      { _id: "buygroceriesid", checked: true, text: "Buy groceries" },
+    ]),
+  } as any)
+  expect(await loadTodos()).toEqual([
+    { checked: false, id: "washcarid", text: "Wash car" },
+    { checked: true, id: "buygroceriesid", text: "Buy groceries" },
+  ])
+})
